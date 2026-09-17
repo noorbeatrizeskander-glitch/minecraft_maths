f7fe20b6d9d4968d64d33c17785b9078073e4f82
noorbeatrizeskander-glitch <noor.beatriz.eskander@gmail.com>
2026-09-17 17:55:50 +0000

    PRIMEIRA APP

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# numero de blocos por pack
+blocos_pack = 64
+
+#funçao para calcular numero de packs + blocos extra
+
+def calculo_packs(n_blocos):
+    n_packs = n_blocos / blocos_pack
+    n_packs_completos = int(n_packs)
+    n_blocos_extra = (n_packs - n_packs_completos) * blocos_pack
+
+    return f"Precisas de {n_packs_completos} packs e {int(n_blocos_extra)} blocos"
+
+blocos = st.slider("Quantos blocos", min_value=0, max_value=10000)
+
+st.write(calculo_packs(blocos))
+    
